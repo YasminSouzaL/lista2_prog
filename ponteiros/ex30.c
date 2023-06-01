c360955521b70ae946b30a8f46d187580919a9fd
Yasmin <souzasmimi@gmail.com>
2023-06-01 15:38:25 -0300

    outro

---
+++ ponteiros/ex30.c
@@ -0,0 +1,9 @@
+/* O que h´a de errado com o programa abaixo?
+i n t main ( ) /∗ e s s e programa e s t a e r r a d o ∗/
+{
+i n t x , ∗p ;
+x = 1 0;
+p = x ;
+p r i n t f (”%d ” , ∗p ) ;
+r e t u r n 0 ;
+}*/

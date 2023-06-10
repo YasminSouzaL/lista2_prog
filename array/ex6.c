324c069f5793011d2d95175b9c0fe3442ec80fc0
Yasmin <souzasmimi@gmail.com>
2023-06-10 19:53:57 -0300

    alt

--- array/ex6.c
+++ array/ex6.c
@@ -5,29 +5,30 @@ e armazenar em um vetor denominado resultado, contendo a pontua¸c˜ao correspon
 aluno. Considere que cada resposta certa equivale a 1 ponto. Exiba a nota dos alunos.*/
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
+
+int main() {
     int i, j, mat[5][10], vet[10], vetA[5], cont=0;
-    for(i=0;i<5;i++){
-        for(j=0;j<10;j++){
+    for(i = 0; i < 5; i++) {
+        for(j = 0; j < 10; j++) {
             printf("Digite o valor da linha %d e coluna %d: ", i+1, j+1);
             scanf("%d", &mat[i][j]);
         }
     }
-    for(i=0;i<10;i++){
-        printf("Digite o %d valor do vetor: ", i+1);
+    for(i = 0; i < 10; i++) {
+        printf("Digite o %d valor do vetor gabarito: ", i+1);
         scanf("%d", &vet[i]);
     }
-    for(i=0;i<5;i++){
-        for(j=0;j<10;j++){
-            if(mat[i][j]==vet[j]){
+    for(i = 0; i < 5; i++) {
+        for(j = 0; j < 10; j++) {
+            if(mat[i][j] == vet[j]) {
                 cont++;
             }
         }
         vetA[i] = cont;
-        cont = 0;
     }
-    for(i=0;i<5;i++){
+    for(i = 0; i < 5; i++) {
         printf("O aluno %d acertou %d questoes.\n", i+1, vetA[i]);
     }
+
     return 0;
 }

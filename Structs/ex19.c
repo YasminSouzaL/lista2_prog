66a64ce16538dc1232dcf94aacc41d1000af21e7
Yasmin <souzasmimi@gmail.com>
2023-06-11 18:51:18 -0300

    alt19

--- Structs/ex19.c
+++ Structs/ex19.c
@@ -4,35 +4,46 @@ Repita este processo at´e que seja lido um valor p = 0.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-struct carro{
-    char marca[15];
+
+struct carro {
+    char marca[16];
     int ano;
     float preco;
 };
-int main(){
+
+int main() {
     struct carro carros[5];
-    for(int i = 0; i < 5; i++){
-        printf("Digite a marca do carro %d: ", i+1);
+
+    for (int i = 0; i < 5; i++) {
+        printf("Digite a marca do carro %d: ", i + 1);
         scanf("%s", carros[i].marca);
-        printf("Digite o ano do carro %d: ", i+1);
+
+        printf("Digite o ano do carro %d: ", i + 1);
         scanf("%d", &carros[i].ano);
-        printf("Digite o preco do carro %d: ", i+1);
+
+        printf("Digite o preco do carro %d: ", i + 1);
         scanf("%f", &carros[i].preco);
     }
+
     float preco;
-    while(1){
+
+    while (1) {
         printf("Digite o preco: ");
         scanf("%f", &preco);
-        if(preco == 0){
+
+        if (preco == 0) {
             break;
         }
-        for(int i = 0; i < 5; i++){
-            if(carros[i].preco < preco){
+
+        for (int i = 0; i < 5; i++) {
+            if (carros[i].preco < preco) {
                 printf("Marca: %s\n", carros[i].marca);
                 printf("Ano: %d\n", carros[i].ano);
                 printf("Preco: %.2f\n", carros[i].preco);
+                printf("\n");
             }
         }
     }
+
     return 0;
 }
